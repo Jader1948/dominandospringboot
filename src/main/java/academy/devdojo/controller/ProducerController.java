7ece820c76777b28ebd688e278bf09236e4dd8e6
jader.marcelino <jader.marcelino1995@gmail.com>
2025-07-27 16:02:56 -0300

    video16: mapping return Objects the manually form .

--- src/main/java/academy/devdojo/controller/ProducerController.java
+++ src/main/java/academy/devdojo/controller/ProducerController.java
@@ -1,6 +1,8 @@
 package academy.devdojo.controller;
 
 import academy.devdojo.domain.Producer;
+import academy.devdojo.request.ProducerPostRequest;
+import academy.devdojo.response.ProducerPostResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
@@ -22,10 +25,18 @@ public class ProducerController {
     // No caso de nao querer retornar nada, no lugar de Producer irá o void e no retorno, noContent.build
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE,
             headers = "x-api-version=v1")
-    public ResponseEntity<Producer> save(@RequestBody Producer producer) {
-        producer.setId(ThreadLocalRandom.current().nextLong(100_000));
+    public ResponseEntity<ProducerPostResponse> save(@RequestBody ProducerPostRequest request) {
+        var producer = Producer.builder()
+                .name(request.getName())
+                .id(ThreadLocalRandom.current().nextLong(100_000))
+                .createAt(LocalDateTime.now())
+                .build();
         Producer.getProducers().add(producer);
-        return ResponseEntity.status(HttpStatus.CREATED).body(producer);
+        var response = ProducerPostResponse.builder()
+                .id(producer.getId())
+                .name(producer.getName())
+                .build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
 }
